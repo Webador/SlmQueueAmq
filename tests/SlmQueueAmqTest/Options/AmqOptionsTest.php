6f61c5ebced187441b6364f37e745e2aab711732
Jurian Sluiman <jurian@soflomo.com>
2015-04-27 22:20:42 +0200

    Add options tests

---
+++ tests/SlmQueueAmqTest/Options/AmqOptionsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SlmQueuedAmqTest\Options;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use SlmQueueAmqTest\Util\ServiceManagerFactory;
+use Zend\ServiceManager\ServiceManager;
+
+class AmqOptionsTest extends TestCase
+{
+    /**
+     * @var ServiceManager
+     */
+    protected $serviceManager;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->serviceManager = ServiceManagerFactory::getServiceManager();
+    }
+
+    public function testCreatedAmqOptions()
+    {
+        /** @var $amqOptions \SlmQueuedAmq\Options\AmqOptions */
+        $amqOptions = $this->serviceManager->get('SlmQueueAmq\Options\AmqOptions');
+
+        $this->assertInstanceOf('SlmQueueAmq\Options\AmqOptions', $amqOptions);
+        $this->assertEquals('tcp://localhost:61613', $amqOptions->getBrokerUrl());
+    }
+}
