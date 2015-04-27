02b89a22a59b052c3ef015d2072b0f280bea084e
Jurian Sluiman <jurian@soflomo.com>
2015-04-27 22:20:30 +0200

    Add controller tests

---
+++ tests/SlmQueueAmqTest/Controller/AmqWorkerControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SlmQueueAmqTest\Controller;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use SlmQueueAmq\Controller\AmqWorkerController;
+use Zend\Mvc\Router\RouteMatch;
+
+class AmqWorkerControllerTest extends TestCase
+{
+    public function testCorrectlyCountJobs()
+    {
+        $queue         = $this->getMock('SlmQueue\Queue\QueueInterface');
+        $worker        = $this->getMock('SlmQueue\Worker\WorkerInterface');
+        $pluginManager = $this->getMock('SlmQueue\Queue\QueuePluginManager', array(), array(), '', false);
+
+        $pluginManager->expects($this->once())
+                      ->method('get')
+                      ->with('newsletter')
+                      ->will($this->returnValue($queue));
+
+        $controller    = new AmqWorkerController($worker, $pluginManager);
+
+        $routeMatch = new RouteMatch(array('queue' => 'newsletter'));
+        $controller->getEvent()->setRouteMatch($routeMatch);
+
+        $worker->expects($this->once())
+               ->method('processQueue')
+               ->with($queue)
+               ->will($this->returnValue(array('One state')));
+
+        $result = $controller->processAction();
+
+        $this->assertStringEndsWith("One state\n", $result);
+    }
+}
