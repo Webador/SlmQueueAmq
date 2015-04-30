4e9f7dd8b9b575ba910125b43d1ee0d5a8b60e6f
Jurian Sluiman <subscribe@juriansluiman.nl>
2015-04-30 09:42:55 +0200

    Add second level autoloader path

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -19,6 +19,7 @@
 
 if  (
     !($loader = @include __DIR__ . '/../vendor/autoload.php')
+    && !($loader = @include __DIR__ . '/../../vendor/autoload.php')
     && !($loader = @include __DIR__ . '/../../../vendor/autoload.php')
 ) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
