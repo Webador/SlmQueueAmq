a146387e6657acacd9dafe900f2833a54b96fe92
Jurian Sluiman <jurian@soflomo.com>
2015-05-06 22:28:33 +0200

    Enable queue push() options
    Add the AMQ options to push jobs into the queue.

--- src/Queue/AmqQueueInterface.php
+++ src/Queue/AmqQueueInterface.php
@@ -6,5 +6,8 @@ use SlmQueue\Queue\QueueInterface;
 
 interface AmqQueueInterface extends QueueInterface
 {
-
+    const DELAY  = 'AMQ_SCHEDULED_DELAY';
+    const PERIOD = 'AMQ_SCHEDULED_PERIOD';
+    const REPEAT = 'AMQ_SCHEDULED_REPEAT';
+    const CRON   = 'AMQ_SCHEDULED_CRON';
 }
