c0f9d631acc270f609f65f3ae4c87315781ed6d7
Jurian Sluiman <jurian@soflomo.com>
2015-05-28 12:44:07 +0200

    Experiment with the Stomp PECL extension

--- src/Service/StompClient.php
+++ src/Service/StompClient.php
@@ -2,12 +2,73 @@
 
 namespace SlmQueueAmq\Service;
 
-use FuseSource\Stomp\Stomp;
+use Stomp;
 
-class StompClient extends Stomp implements StompClientInterface
+class StompClient implements StompClientInterface
 {
+    protected $subscriptions = [];
+    protected $stomp;
+    protected $broker;
+
+    public function __construct($broker)
+    {
+        $this->broker = $broker;
+    }
+
+    public function isConnected()
+    {
+        return (null !== $this->stomp);
+    }
+
+    public function connect($username = '', $password = '')
+    {
+        $this->stomp = new Stomp($this->broker, $username, $password);
+    }
+
+    public function send($destination, $message, $properties = [], $sync = null)
+    {
+        $this->stomp->send($destination, $message, $properties);
+    }
+
+    public function subscribe($destination, $properties = [], $sync = null)
+    {
+        $this->stomp->subscribe($destination, $properties);
+        $this->subscriptions[$destination] = $properties;
+    }
+
     public function getSubscriptions()
     {
-        return $this->_subscriptions;
+        return $this->subscriptions;
+    }
+
+    public function unsubscribe($destination, $properties = [], $sync = null)
+    {
+        $this->stomp->unsubscribe($destination, $properties);
+    }
+
+    public function ack($message, $transactionId = null)
+    {
+        $headers = ($transactionId) ? ['transaction' => $transactionId] : [];
+        $this->stomp->ack($message, $headers);
+    }
+
+    public function disconnect()
+    {
+        unset($this->stomp);
+    }
+
+    public function readFrame()
+    {
+        return $this->stomp->readFrame();
+    }
+
+    public function setReadTimeout($seconds, $milliseconds = 0)
+    {
+        $this->stomp->setReadTimeout($seconds, $milliseconds);
+    }
+
+    public function hasFrameToRead()
+    {
+        return $this->stomp->hasFrame();
     }
 }
--- src/Service/StompClientInterface.php
+++ src/Service/StompClientInterface.php
@@ -10,9 +10,6 @@ interface StompClientInterface
     public function send($destination, $message, $properties = [], $sync = null);
     public function subscribe($destination, $properties = [], $sync = null);
     public function unsubscribe($destination, $properties = [], $sync = null);
-    public function begin($transactionId = null, $sync = null);
-    public function commit($commit = null, $sync = null);
-    public function abort($transactionId = null, $sync = null);
     public function ack($message, $transactionId = null);
     public function disconnect();
     public function readFrame();
